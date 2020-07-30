8267e88e61511ff39c0e0dc2038ddd3efea14077
Alex Nicholas <al.nicholas@gmail.com>
2020-07-30 09:27:24 +0100

    markdown parsing

--- frontend/src/components/RedditComment.js
+++ frontend/src/components/RedditComment.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Avatar } from '@material-ui/core'
 import { randomCage } from '../utils/randomCage'
 import moment from 'moment'
+import ReactMarkdown from 'react-markdown'
 
 const RedditComment = ({ comment }) => {
 
@@ -13,7 +14,7 @@ const RedditComment = ({ comment }) => {
         subheader={moment(comment.reddit_created_at).fromNow()}
       />
       <CardContent>
-        <p>{comment.body}</p>
+        <ReactMarkdown source={comment.body} />
       </CardContent>
     </Card>
   )
